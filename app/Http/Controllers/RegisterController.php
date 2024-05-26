338e099f4d03ea14c9efe69dd80cb1808c3ae309
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-26 23:40:42 +0200

    Refactor the code to the laravel way

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -17,16 +18,14 @@ class RegisterController extends Controller
             "password" => "string|min:5"
         ]);
         $password = Hash::make($request->input('password'));
-        $is_inserted = DB::select("CALL register_new_user(:email, :password, :first_name, :last_name)", [
-            "email"=> $request->input("email"),
-            "password"=> $password,
-            "first_name"=> $request->input("first_name"),
-            "last_name"=> $request->input("last_name"),
+        $user = User::create([
+            'last_name' => $request->input('last_name'),
+            'first_name' => $request->input('first_name'),
+            'email' => $request->input('email'),
+            'password' => $password,
         ]);
-        if(empty($is_inserted))
-        {
-            return redirect("/register")->withErrors(["mistake"=>"So"]);
-        }
+
+        session(["user_id" => $user->id_user]);
         return view("register.register_page", ["success"=>"You successfully register"]);
     }
 
