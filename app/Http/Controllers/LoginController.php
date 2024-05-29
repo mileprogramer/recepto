bdbc0b7c503c6d5edf563264570627460d5089b0
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-29 22:23:09 +0200

    Add admin middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('/');
+            return redirect()->intended();
         }
     
         return redirect()->back()->withErrors(['mistake' => 'Invalid credentials']);
