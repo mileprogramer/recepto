88133495c7d790e35c235e25ef100746dfac3f25
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-29 22:21:52 +0200

    Add delete button to delete ingredients

--- resources/js/addIngredient.js
+++ resources/js/addIngredient.js
@@ -1,11 +1,29 @@
 let btn = document.getElementById("add-ingredient");
 let ingredients = document.querySelector("#ingredients-container");
 (btn !== null)? btn.addEventListener("click", addIngredinet) : null;
+let btnsDeleteAddedIngredient = null;
 
+let counter = 0;
 function addIngredinet(){
-    let input = document.createElement("input");
-    input.classList.add("form-control");
-    input.name = "ingredients[]";
-    input.text = "text";
-    ingredients.append(input);
+    let html = `
+            <input type="text" class="form-control my-3" name='ingredients[]'>
+            <input type="button" class="btn btn-danger my-3" value='Delete ingredient'>
+    `;
+    let divParent = document.createElement("div");
+    divParent.classList.add("row", "gap-3");
+    divParent.innerHTML = html;
+    ingredients.append(divParent);
+    btnsDeleteAddedIngredient = ingredients.querySelectorAll(".btn-danger");
+    addDeleteClicks();
+}
+
+function addDeleteClicks(){
+    if(!btnsDeleteAddedIngredient.length) return ;
+    btnsDeleteAddedIngredient.forEach(btn => {
+        btn.onclick = function(){
+            let parentElement = this.parentElement;
+            ingredients.removeChild(parentElement);
+            btnsDeleteAddedIngredient = ingredients.querySelectorAll(".btn-danger");
+        }
+    });
 }
--- resources/js/addMoreIngredients.js
+++ resources/js/addMoreIngredients.js
@@ -1,6 +1,8 @@
 let btn = document.getElementById("add-ingredient");
 let ingredients = document.querySelector("#ingredients-container");
 let ingredientsSelect = document.querySelector("#ingredientUnit");
+let btnsDeleteAddedIngredient = ingredients.querySelectorAll(".btn-danger");
+addDeleteClicks();
 
 (btn !== null)? btn.addEventListener("click", addIngredinet) : null;
 
@@ -17,9 +19,25 @@ function addIngredinet(){
                 ${ingredientsSelect.innerHTML.trim()}
             </select>
         </div>
+        <div class="col-2">
+            <input class="w-100 btn btn-danger w-10 col-4" type='button' value='Delete ingredient'/>
+        </div>
     `;
     let div = document.createElement("div");
     div.classList.add("row", "pt-3");
     div.innerHTML = html;
     ingredients.appendChild(div);
+    btnsDeleteAddedIngredient = ingredients.querySelectorAll(".btn-danger");
+    addDeleteClicks();
+}
+
+function addDeleteClicks(){
+    if(!btnsDeleteAddedIngredient.length) return ;
+    btnsDeleteAddedIngredient.forEach(btn => {
+        btn.onclick = function(){
+            let parentElement = this.parentElement.parentElement;
+            ingredients.removeChild(parentElement);
+            btnsDeleteAddedIngredient = ingredients.querySelectorAll(".btn-danger");
+        }
+    });
 }
